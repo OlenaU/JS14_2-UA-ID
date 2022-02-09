b725e962669d53cc22494d7a323e5f8554863a75
OlenaU <udalaolena@gmail.com>
2022-02-09 01:34:45 -0800

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,50 @@
 
 //let id = 2883616949; 
+
 const maincheck  = [-1, 5, 7, 9, 4, 6, 10, 5, 7];
-const date_time = {
-  timezone: 'UTC',
-  year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-};
 
 function IDcheck(){
-
-let inputcode = document.getElementById('xvar').value.split("");
+  let inputcode = document.getElementById('xvar').value.split("")
+  console.log(inputcode);
 
 let result = 0;
-for (let i=0; i < inputcode.length - 1; i++) {
+for (let i = 0; i < inputcode.length - 1; i++) {
   result += (inputcode[i] * maincheck[i]);
 }
+
 let ControlNumber = result - (Math.floor(result / 11) * 11);
 console.dir(ControlNumber);
 
-if (ControlNumber != inputcode[9]) {
-  return alert('Введенный вами ИНН некорректен');
-} else
-return alert('Введенный вами ИНН корректен');
+if (ControlNumber == inputcode[9]) {
+  
+  let gender = inputcode[8] % 2 == 0 ? 'female' : 'male';
+  console.log (gender);
+  let startday = new Date(1899, 12, 31);
+  startday.setDate(inputcode.splice(0, 5).join(""));
+  
+  let now = new Date();
+  let diff = 0;
+  diff = now.getFullYear() - startday.getFullYear();
+  
+
+  if (now.getMonth() < startday.getMonth()) 
+  {
+      console.log(diff -= 1);
+  }
+  else if (now.getMonth() == startday.getMonth()) 
+  {
+      if (now.getDate() < startday.getDate()) 
+      {
+          console.log(diff -= 1);
+      }
+  }
+
+  return alert(`The ID is valid. The Birtdate of ID owner is ${startday}.The full age is ${diff}. The Gender is ${gender} ` ); //
+}
+else {
+return alert(`The ID is not valid`);
+
 }
 
+}
 
-
-//const BD = id.splice(4);
-//console.dir(BD);
-
-//let StartDate = (1899, 12, 31, 0, 0, 0);
-
-//
-//let checkBD = 
-//let checksex = 
-//console.log(arrid);
